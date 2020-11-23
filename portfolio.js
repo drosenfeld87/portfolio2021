32520e1c8a8949b8bc456728477da93d122ad856
Debbie Rosenfeld <drosenfeld@ITRG-drosenfeld.local>
2020-11-23 08:52:42 -0500

    started sticky navbar and intial scss setup

---
+++ portfolio.js
@@ -0,0 +1,11 @@
+// $(document).ready(function() {
+//     $(window).scroll(function() {
+// 		var windowpos = $(window).scrollTop();
+//         var navbar = $('#navbar');
+//         if (windowpos >= navbar.position().top && !navbar.hasClass("fixed-nav-bar") ) {
+//         	navbar.addClass("fixed-nav-bar");
+//         } else if( windowpos <= navbar.position().top && navbar.hasClass("fixed-nav-bar")  ) {
+//             navbar.removeClass("fixed-nav-bar");
+//         }
+//     });
+// });
